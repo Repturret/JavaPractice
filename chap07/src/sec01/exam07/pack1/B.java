7d7d844e97dbdce4430febb5fb1004a04ea4793f
Repturret <mymiwltjd@naver.com>
2025-09-24 11:46:17 +0900

    09/24

---
+++ chap07/src/sec01/exam07/pack1/B.java
@@ -0,0 +1,11 @@
+package sec01.exam07.pack1;
+
+public class B {
+	
+	public void method() { // 가능
+		A a = new A();
+		a.field = "value";
+		a.method();
+	}
+
+}
---
+++ chap07/src/sec01/exam07/pack2/D.java
@@ -0,0 +1,13 @@
+package sec01.exam07.pack2;
+
+import sec01.exam07.pack1.A;
+
+public class D extends A {
+	
+	public D() { // 가능
+		super();
+		this.field = "value";
+		this.method();
+	}
+
+}
